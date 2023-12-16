1a4165a7cb54e23e361be8bb286f648ad48ffae7
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-12-16 11:11:37 +0800

    fix: allow different path splitors

--- json2dataframe.py
+++ json2dataframe.py
@@ -6,7 +6,7 @@ from util import translate_one
 def main():
     """這邊示範把當前目錄下的所有json檔合併成一個DataFrame，然後每3個column print一次"""
     
-    df = pd.DataFrame([translate_one("./raw_weather_data/" + file_name) for file_name in os.listdir("./raw_weather_data") if file_name.endswith(".json")])
+    df = pd.DataFrame([translate_one(os.path.join(".", "raw_weather_data", file_name)) for file_name in os.listdir(os.path.join(".", "raw_weather_data")) if file_name.endswith(".json")])
 
     for i in range(0, len(df.columns), 3):
         print(df.iloc[:, i:i+3])
